0748b46ea537f0547a913fdd26f0b3b10a17b354
ngbontsi <ngbontsi@gmail.com>
2025-04-24 04:52:18 +0200

    refining the project

--- src/main/java/za/co/ngbontsi/api/Main.java
+++ src/main/java/za/co/ngbontsi/api/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package za.co.ngbontsi.api;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
