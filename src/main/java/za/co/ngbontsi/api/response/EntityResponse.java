d595d9eb4a2594ee35efea61135d447b4bd495db
ngbontsi <ngbontsi@gmail.com>
2025-04-24 07:07:42 +0200

    fixing test errors

--- src/main/java/za/co/ngbontsi/api/response/EntityResponse.java
+++ src/main/java/za/co/ngbontsi/api/response/EntityResponse.java
@@ -2,12 +2,7 @@ package za.co.ngbontsi.api.response;
 
 public class EntityResponse<T> {
 
-    final private String message;
-    final private T data;
-
     public EntityResponse(int statusCode, String message, T data) {
-        this.message = message;
-        this.data = data;
     }
 
     public EntityResponse(int statusCode, String message) {
